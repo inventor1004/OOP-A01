8246d64bf148f05e410ab2493977c6a996b2ce43
inventor1004 <117550785+inventor1004@users.noreply.github.com>
2023-05-11 09:18:08 -0400

    The header comment is added

---
+++ assign1.cpp
@@ -0,0 +1,13 @@
+/* Filename: assign1.cpp
+ * By: Honggyu(Chris) Park
+ * Date: 05/11/2023
+ * Description: This program designed to calculate the average of students' grades
+               and determinds whether they pass the corse or not.
+                The program can take four different types of the input data depending on the 
+               user's purpose.
+               -1. Maximum 5 different numberical numbers
+               -2. Maximum 5 different letter grades 
+               -3. Special letters (I, Q, AU, DNA, I/P) for the certain student's status 
+               -4. The name of file which it includes the information of the student's grade
+                The user can exit the program entering the 'X'
+ */
